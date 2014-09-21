b4977e0809600b5729839ffb4b67f8a802881377
ipl2fnf <ipl2fnf@gmail.com>
2014-09-21 01:02:18 -0500

    Execute this after installation.

---
+++ install.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install default-jdk
+sudo apt-get install build-essential checkinstall
+sudo apt-get install git
+sudo apt-get install cmake
+sudo apt-get install gnome-devel
+sudo apt-get install flashplugin-installer
